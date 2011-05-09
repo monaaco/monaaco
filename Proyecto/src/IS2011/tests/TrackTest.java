e0f3164f5ff1a9dd0e7c1fa538fee59166715b70
beatorresal <beatorresal@gmail.com>
2011-05-09 16:12:25 +0000

    test clase Track

--- Proyecto/src/IS2011/tests/TrackTest.java
+++ Proyecto/src/IS2011/tests/TrackTest.java
@@ -1,37 +1,97 @@
 package IS2011.tests;
 
-import java.io.File;
-
-import java.util.*;
-
 import IS2011.bibliotecaXML.*;
-import javazoom.jlgui.basicplayer.BasicPlayer;
 import javazoom.jlgui.basicplayer.BasicPlayerException;
 import junit.framework.TestCase;
 
 public class TrackTest extends TestCase {
-
-	public void testToString(){
+	
+	private Track track;
+	private String cadena;
+	private Playlist listaReproduccion;
+	
+	public TrackTest(){
+		super();
+		try {
+			this.setUp();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void setUp(){
+		listaReproduccion = new Playlist();
+		listaReproduccion.setRepeat(true);
+		listaReproduccion.add("sounds/prueba.mp3"); 
+		
 		String ruta = "sounds/prueba.mp3";
-		Track track = new Track(ruta);
-		String cadena = "Artist: Rafael Lechowski\n Name: Donde duele inspira\n Time: 216\n";
+		track = new Track(ruta);
+		cadena = "Artist: Flowklorikos\n Name: Donde duele inspira\n Time: 216\n";
+	}
+	public void testToString(){
+		
+	
+		
 		assertEquals(cadena, track.toString());		
 	}
 	
 	public void testArtist() throws BasicPlayerException {
-		Playlist listaReproduccion = new Playlist();
-		listaReproduccion.setRepeat(true);
-		listaReproduccion.add("sounds/prueba.mp3"); 
+		
+		
+		
 
-		assertEquals(listaReproduccion.current().getArtist(),"Rafael Lechowski");
+		assertEquals(listaReproduccion.current().getArtist(),"Flowklorikos");
 	}
-	public void testAlbum() throws BasicPlayerException {
-		Playlist listaReproduccion = new Playlist();
-		listaReproduccion.setRepeat(true);
-		listaReproduccion.add("sounds/prueba.mp3"); 
-		System.out.println(listaReproduccion.current().getName());
+	public void testGetTotalTime(){
+	
+		
+
+		
+		assertEquals(listaReproduccion.current().getTotalTime(),(Integer)Integer.parseInt("216"));
+	}
+	public void testGetAlbum() throws BasicPlayerException {
+	
+ 
+	
 		assertEquals(listaReproduccion.current().getName(),"Donde duele inspira");
 	}
 	
+	public void testGetAlmbumArtist(){
 
+		
+
+		assertEquals(listaReproduccion.current().getAlbumArtist(),"prueba");
+	
+	}
+	
+	public void testGetGener(){
+		
+
+		assertEquals(listaReproduccion.current().getGenre(),"Hip-Hop");
+		
+	}
+	
+	public void testGetKind(){
+	
+	
+		
+		assertEquals(listaReproduccion.current().getKind(),"MPEG-1 Layer 3");
+		
+	}
+	
+	
+	public void testGetComments()throws BasicPlayerException {
+		
+		
+		
+		assertEquals(listaReproduccion.current().getComments(),"www.HHGroups.com");
+	
+	}
+	public void testGetBitrate(){
+
+		
+		
+		assertEquals(listaReproduccion.current().getBitRate(),(Integer)Integer.parseInt("320"));
+	}
+	
 }
